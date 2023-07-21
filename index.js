2fdcdf100be89f0f544ff584793cb8d288f7d93f
Byron <byron.mena.7@gmail.com>
2023-07-21 02:43:32 -0300

    Se ha creado asistente IA que responde con la voz a preguntas particulares, sin memoria

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
 app.use("/", require("./routes/api"));
+app.use("/audios", express.static(path.join(__dirname, "audios")));
 
 app.use(function (err, req, res, next) {
   res.status(422).send({ error: err.message });
--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,20 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+//Texto a voz
+const path = require("path");
+const voice = require("elevenlabs-node");
+const fs = require("fs-extra");
+const player = require("play-sound")((opts = {}));
+
+const apiKey = process.env.ELEVENLABS_API_KEY; // Your API key from Elevenlabs
+const voiceID = "MF3mGyEYCl7XYWbV9V6O"; // The ID of the voice you want to get
+const fileName = "audio.mp3"; // The name of your audio file
+const modelID = "eleven_multilingual_v1";
+const stability = 0;
+const similarity_boost = 0;
+//
+
 async function transcribe(buffer) {
   const response = await openai.createTranscription(
     buffer, // El archivo de audio que se va a transcribir.
@@ -44,15 +58,53 @@ router.post("/", upload.any("file"), async (req, res) => {
   try {
     const transcriptionResponse = await transcribe(buffer);
     const transcriptionText = transcriptionResponse.data.text;
+    console.log("Solicitud: ", transcriptionText);
     const chatResponse = await chat(transcriptionText);
-    res.send({
-      type: "POST",
-      transcription: chatResponse.data.choices[0].message.content,
-      audioFileName: buffer.name,
-    });
+    const textInput = chatResponse.data.choices[0].message.content;
+    console.log("Respuesta: ", chatResponse.data.choices[0].message.content);
+
+    const audioPath = path.join(__dirname, "../audios", "audio.mp3");
+    voice
+      .textToSpeechStream(
+        apiKey,
+        voiceID,
+        textInput,
+        stability,
+        similarity_boost,
+        modelID
+      )
+      .then((voiceStream) => {
+        const writeStream = fs.createWriteStream(audioPath);
+        voiceStream.pipe(writeStream);
+
+        writeStream.on("finish", () => {
+          res.send({
+            type: "POST",
+            transcription: chatResponse.data.choices[0].message.content,
+            audioURL: `/audios/audio.mp3`, // Enviar la URL del archivo de audio al cliente
+          });
+        });
+      })
+      .catch((err) => {
+        console.error("Error al generar el audio:", err);
+        res.send({ type: "POST", message: err });
+      });
   } catch (err) {
+    console.log("Error al enviar el POST", err.message);
     res.send({ type: "POST", message: err });
   }
 });
+
+const generateAudio = (text, audioFileName) => {
+  return new Promise((resolve, reject) => {
+    say.export(text, "Microsoft David Desktop", 1, audioFileName, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
 
 module.exports = router;
